316e14e5f327f2e9cc673bb24e9d89da3ccb8c12
Andrey Dubinchik <evil_ghost@bk.ru>
2023-04-05 15:51:02 +0300

    delete pytest.mark decorator (#2)

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,9 +1,6 @@
 import os
 
-import pytest
 
-
-@pytest.mark.dependency()
 def test_project_folder_in_place(root_dir, project_dirname):
     manage_rpath = os.path.join(project_dirname, 'manage.py')
     manage_fpath = os.path.join(root_dir, manage_rpath)
