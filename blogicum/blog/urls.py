82ad5a132632141c419c552be3d367616e612fa5
Sibirichin Kirill <vulpes87_1@mail.ru>
2024-01-16 23:15:24 +0500

    update 1

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -11,6 +11,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
     path('posts/<int:id>/', post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/',
-          category_posts, name='category_posts'),
+    path('category/<slug:category_slug>/', category_posts,
+         name='category_posts'),
 ]
