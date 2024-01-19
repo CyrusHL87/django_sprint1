bdce295f221185c58862ff2db018f4c8241a839e
Sibirichin Kirill <vulpes87_1@mail.ru>
2024-01-19 09:55:54 +0500

    Испр 5 19 01 2024

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin 
-from django.urls import include, path 
- 
-urlpatterns = [ 
-    path('admin/', admin.site.urls), 
-    path('pages/', include('pages.urls')), 
-    path('', include('blog.urls')), 
-] 
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('pages/', include('pages.urls')),
+    path('', include('blog.urls')),
+]
